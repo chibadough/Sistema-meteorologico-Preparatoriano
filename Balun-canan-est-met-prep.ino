c4b33a2552606fde5ab6d1e46172e56fba63f5be
chibadough <jpespi581@gmail.com>
2026-05-21 16:35:32 -0600

    Update Balun-canan-est-met-prep.ino
    *actualizacion*
    se introdujo un sistema de aviso cuando el dht11 falle, cuando esto suceda se reemplazará con otro de respaldo y en caso ambos fallen el sistema manda un aviso de revision. por otra parte el bmp280 tiene un sistema similar, pero este simplemente manda el aviso a revision puesto que no se cuenta con un reemplazo por el momento.
    
    *Update*
    A notification system has been introduced to alert users when the DHT11 fails. When this happens, it will be replaced with a backup unit, and if both fail, the system will send a service notification. The BMP280 has a similar system, but it simply sends a service notification since a replacement is not currently available.

--- Balun-canan-est-met-prep.ino
+++ Balun-canan-est-met-prep.ino
@@ -4,14 +4,21 @@
 #include <DHT.h>
 #include <DHT_U.h>
 int pinV =7;
+int pinV2 =4;
 
 
 Adafruit_BMP280 bmp;
 
+bool advertencia1 = false;
+bool advertencia2 = false;
+
   #define DHTPIN 5
+  #define DHTPIN1 8
   #define DHTTYPE DHT11
 
+
 DHT dht(DHTPIN,DHTTYPE);
+DHT dht1(DHTPIN1,DHTTYPE);
 
   float temp;
   float pres;
@@ -20,9 +27,11 @@ DHT dht(DHTPIN,DHTTYPE);
 void setup() {
  Serial.begin(9600);
  dht.begin();
+ dht1.begin();
  Wire.begin();
  bmp.begin(0x76);
   pinMode(pinV,OUTPUT);
+  pinMode(pinV2,OUTPUT);
 }
 
 void loop() {
@@ -30,15 +39,35 @@ void loop() {
   float hum1 = dht.readHumidity();
  temp = bmp.readTemperature();
  pres = bmp.readPressure()/100;
+ 
 
- if(isnan(tem1) || isnan(hum1)){
- Serial.print("Reinciando EL dht11");
- digitalWrite(pinV,LOW);
+ if((isnan(tem1) || isnan(hum1))&& advertencia1 == false){
+ advertencia1 = true;
+ Serial.println("Reinciando EL dht11");
+ digitalWrite(pinV, LOW);
  delay(1000);
- digitalWrite(pinV,HIGH);
- } else{
- digitalWrite(pinV,HIGH);
+ digitalWrite(pinV, HIGH);
+ delay(1000);
+  tem1 = dht.readTemperature();
+  hum1 = dht.readHumidity();
+ } else if ((isnan(tem1) || isnan(hum1)) && advertencia1 == true && advertencia2 == false){
+   Serial.println("Cambiando a dht11 de repuesto");
+   advertencia2 = true;
+   digitalWrite(pinV2, HIGH);
+   tem1 = dht1.readTemperature();
+   hum1 = dht1.readHumidity();
+ } else if ((isnan(tem1) || isnan(hum1)) && advertencia2 == true){
+ Serial.println("PROBLEMA CON EL DHT11: REVISAR");
+ } else {
+ digitalWrite(pinV, HIGH);
+ digitalWrite(pinV2, HIGH);
  }
+
+ if (isnan(temp) || isnan(pres)){
+  Serial.println("ERROR CON EL BMP280: REVISAR PUESTO QUE NO HAY REPUESTO");
+ }
+
+
   
   Serial.println("=====Datos generales=====");
   Serial.print("temperatura actual: ");
@@ -62,3 +91,4 @@ void loop() {
  delay(4000);
 
  }
+
